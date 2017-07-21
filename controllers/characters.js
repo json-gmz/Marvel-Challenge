8f7f4f9c9b0d2c9dd317edd9d0aec55c58017205
JsonGmz <jeisson.gomez@ingeniocontenido.co>
2017-07-21 18:45:42 -0500

    Ajuste paginado y buscador

--- controllers/characters.js
+++ controllers/characters.js
@@ -10,9 +10,24 @@ $(function() {
     });
 });
 
+function nextPage(){
+    offsetdefault = offsetdefault + 10;
+    getCharacters();
+    window.scrollTo(0, 0);
+}
+
+function prevPage(){
+    offsetdefault = offsetdefault - 10;
+    getCharacters();
+    window.scrollTo(0, 0);
+}
+
 function sortBy(sort){
     $(".sortbyoption").removeClass("active");
     $("#sortby"+sort).addClass("active");
+    offsetdefault = 0;
+    orderbydefault = sort;
+    getCharacters();
 }
 
 function getCharacters(sort = ""){
@@ -24,13 +39,18 @@ function getCharacters(sort = ""){
         data: { action: "getmd5", ts: timestamp, privateKey: privatekey, publicKey: publickey }
     }).done(function( hash ) {
         if ( hash != "" && hash != 0 ) {
+            
+            if ( offsetdefault < 0 ) {
+                offsetdefault = 0;
+            }
 
             var parameters = {};
             parameters["apikey"] = publickey;
             parameters["ts"] = timestamp;
             parameters["hash"] = hash;
             parameters["limit"] = 10;
-            parameters["orderBy"] = "-modified";
+            parameters["orderBy"] = orderbydefault;
+            parameters["offset"] = offsetdefault;
             
             if ( $("#search-character").val() != "" ) {
                 parameters["nameStartsWith"] = $("#search-character").val();
@@ -47,28 +67,40 @@ function getCharacters(sort = ""){
                 data: parameters
             }).done(function( response ) {
                 var characters = response.data.results;
-                console.log(characters);
                 var html = "";
                 if ( characters.length > 0 ) {
                     $.each( characters, function( key, data ) {
+                        
+                        var htmlcomic = "";
+                        if ( data.comics.items.length > 0 ) {
+                            var countcomic = 0;
+                            $.each( data.comics.items, function( key, comic ) {
+                                if ( countcomic < 4 ) {
+                                    htmlcomic = htmlcomic + "<div class='col-sm-6 related-comic'>"+comic.name+"</div>";
+                                    countcomic = countcomic + 1;
+                                }
+                            });
+                        } else {
+                            htmlcomic = "<div class='col-sm-12 text-center not-founds'>No related comics :(</div>";
+                        }
+                        
                         html = html + "<div class='col-sm-6 container-char'>\
                                             <div class='row'>\
-                                                <div class='col-sm-6'>\
-                                                    <img src='./front/img/icons/characters.png'>\
+                                                <div class='col-sm-5'>\
+                                                    <img class='img-character' src='"+data.thumbnail.path+"."+data.thumbnail.extension+"'>\
                                                 </div>\
-                                                <div class='col-sm-6'>\
+                                                <div class='col-sm-7'>\
                                                     <div class='name-char'>"+data.name+"</div>\
-                                                    <div class='description-short-char'>"+data.description.slice(0,150)+"...</div>\
+                                                    <div class='description-short-char'>"+ (data.description!="" ? data.description.slice(0,250) : "Description not available") +"...</div>\
                                                     <div class='button-view-more'>VIEW MORE</div>\
                                                 </div>\
                                             </div>\
                                             <div class='row'>\
                                                 <div class='col-sm-12'>\
                                                     <div class='col-sm-12 related-comics'>Related comics</div>\
-                                                    <div class='col-sm-6'>comic 1</div>\
-                                                    <div class='col-sm-6'>comic 2</div>\
-                                                    <div class='col-sm-6'>comic 3</div>\
-                                                    <div class='col-sm-6'>comic 4</div>\
+                                                    <div class='col-sm-12 '>\
+                                                        "+htmlcomic+"\
+                                                    </div>\
                                                 </div>\
                                             </div>\
                                         </div>";
--- data/config.js
+++ data/config.js
@@ -1,2 +1,4 @@
 var publickey = "bd0887227461e3a241bbec58b0f49366";
 var privatekey = "dfb3f95c46ce878caf55ec89d14abaf2f64bb105";
+var offsetdefault = 0;
+var orderbydefault = "name";
