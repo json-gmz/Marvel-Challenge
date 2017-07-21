35ddc4a3fd838476cabf723ad047d4022aad2fbe
jeisson <jeisson@json-pc>
2017-07-21 01:20:04 -0500

    Bloque Personajes

--- controllers/characters.js
+++ controllers/characters.js
@@ -8,10 +8,36 @@ $(function() {
         if ( hash != "" && hash != 0 ) {
             $.ajax({
                 method: "GET",
-                url: "http://gateway.marvel.com/v1/public/comics",
-                data: { apikey: publickey, ts: timestamp, hash: hash }
+                url: "http://gateway.marvel.com/v1/public/characters",
+                data: { apikey: publickey, ts: timestamp, hash: hash, orderBy: "name", limit: 10 }
             }).done(function( response ) {
-                console.log(response);
+                var characters = response.data.results;
+                console.log(characters);
+                var html = "";
+                $.each( characters, function( key, data ) {
+                    html = html + "<div class='col-sm-6 container-char'>\
+                                        <div class='row'>\
+                                            <div class='col-sm-6'>\
+                                                <img src='./front/img/icons/characters.png'>\
+                                            </div>\
+                                            <div class='col-sm-6'>\
+                                                <div class='name-char'>"+data.name+"</div>\
+                                                <div class='description-short-char'>"+data.description.slice(0,150)+"...</div>\
+                                                <div class='button-view-more'>VIEW MORE</div>\
+                                            </div>\
+                                        </div>\
+                                        <div class='row'>\
+                                            <div class='col-sm-12'>\
+                                                <div class='col-sm-12 related-comics'>Related comics</div>\
+                                                <div class='col-sm-6'>comic 1</div>\
+                                                <div class='col-sm-6'>comic 2</div>\
+                                                <div class='col-sm-6'>comic 3</div>\
+                                                <div class='col-sm-6'>comic 4</div>\
+                                            </div>\
+                                        </div>\
+                                    </div>";
+                });
+                $("#block-characters").html(html);
             })
             .fail(function() {
                 alert("Error loading characters.");
